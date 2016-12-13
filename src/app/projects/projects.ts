186388ce2e23ad8250cd30fbb18a9722667b0737
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-12-12 22:02:26 -0800

    add scarp.io project

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -33,7 +33,7 @@ export const PROJECTS: Project[] = [
     id: 3,
     name: 'Angular Shopping Cart',
     description: 'An AngularJS 1 exercise (I have since moved on to Angular 2), the goal was to build a single-page shopping cart application with a shared service across components. Users can use filter/search options to find the desired product, then add a certain quantity to their shopping bag and proceed to the checkout view, which displays the order total and each item with quantity and subtotal. They can then remove or edit the quantity of each item in their shopping cart.',
-    technologies: 'AngularJS 1, JSON, HTML, CSS',
+    technologies: 'JavaScript, Angular, JSON, HTML, CSS',
     screenshots: [
       '/assets/images/shopping-cart-1.png',
       '/assets/images/shopping-cart-2.png',
@@ -46,15 +46,40 @@ export const PROJECTS: Project[] = [
   {
     id: 4,
     name: 'SommBuddy',
-    description: 'An AngularJS 1 exercise (I have since moved on to Angular 2), the goal was to build a single-page shopping cart application with a shared service across components. Users can use filter/search options to find the desired product, then add a certain quantity to their shopping bag and proceed to the checkout view, which displays the order total and each item with quantity and subtotal. They can then remove or edit the quantity of each item in their shopping cart.',
-    technologies: 'AngularJS 1, JSON, HTML, CSS',
+    description: 'A group project at Galvanize, this is a wine-pairing mobile application that takes several input parameters from the user such as meal type, preferred flavor profile, and price range, then returns three new wines that fit those characteristics. Users can save a wine to a favorites list. After the planning and architecture phase of the project, my main role was setting up the intermediate API that received the initial user input. This service took the wine.com API results and applied further filtering and object restructuring, crawled the image URL\'s page for the actual wine bottle image (the response object included only a picture of the label or a URL to a page with the picture of the bottle), and sent the final response back to the client application.',
+    technologies: 'Node.js, Ionic, Angular, wine.com API',
     screenshots: [
-      '/assets/images/shopping-cart-1.png',
-      '/assets/images/shopping-cart-2.png',
-      '/assets/images/shopping-cart-3.png',
-      '/assets/images/shopping-cart-4.png'
+      '/assets/images/sommbuddy-1.png',
+      '/assets/images/sommbuddy-2.png',
+      '/assets/images/sommbuddy-3.png',
+      '/assets/images/sommbuddy-4.png',
+      '/assets/images/sommbuddy-5.png',
+      '/assets/images/sommbuddy-6.png'
     ],
     gitHubUrl: 'https://github.com/discoverychannel/sommbuddy-server',
+    deploymentUrl: ''
+  },
+  {
+    id: 5,
+    name: '8-K Crawler',
+    description: 'A Python crawler that parses SEC filings of various companies. Hundreds of 8-K (current report) documents are filed with the SEC every day, but I\'m only interested in the ones that contain the topic of "Other Events". Going through all the 8-Ks and checking for this topic is time-consuming and involves a lot of clicks, so I built a command line program that crawls the financial statements database, finds the right filings, proceeds to their content, and presents them to me on new browser tabs. The program uses a simple file read/write (not the most robust system but it does the job for the scale of this program) to track which filing was last viewed, so as the database gets new filings throughout the day, the user can check periodically and only get the filings they haven\'t seen yet.',
+    technologies: 'Python, BeautifulSoup',
+    screenshots: [
+      '/assets/images/8k-crawler-1.png',
+      '/assets/images/8k-crawler-2.png'
+    ],
+    gitHubUrl: 'https://github.com/spencerscarpelli/filings-crawler',
+    deploymentUrl: ''
+  },
+  {
+    id: 6,
+    name: 'scarp.io',
+    description: 'Yes, this website! Very meta. In addition to showcasing some projects and hosting blog posts, I figured it would be good to work with Angular 2 and build a site from scratch. I will also build the "engine" for blogging and posting additional projects, so that I can conveniently add content and it will populate my database for this website to pull from. I could of course use something like Wordpress, but I\'ll enjoy the process of building the engine myself, linking the database to the applications, and scaling it to my needs.',
+    technologies: 'Angular, Angular CLI, TypeScript, SVG, HTML, CSS',
+    screenshots: [
+      '/assets/images/ss-1.png'
+    ],
+    gitHubUrl: 'https://github.com/spencerscarpelli/spencer-scarpelli',
     deploymentUrl: ''
   }
 ]
